284135b4c084dd83f76c58b17cf486d211217b3d
bc <bcl@nclasters.org>
2022-07-25 14:02:10 -0400

    updates for monitoring

---
+++ extra/setup-monitoring.sh
@@ -0,0 +1,28 @@
+echo ...Creating namespace
+kubectl create ns monitoring
+echo 
+echo ...Installing Prometheus and Grafana
+echo
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+helm install -n monitoring monitoring prometheus-community/kube-prometheus-stack
+echo
+echo ...Installing Kubernetes dashboard
+echo
+# install dashboard
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.5.0/aio/deploy/recommended.yaml
+kubectl apply -f ~/k8s-dev/monitoring/dashboard-rbac.yaml
+echo
+echo ---- TOKEN to use for logging into dashboard follows ---
+echo 
+~/k8s-dev/monitoring/get-token.sh
+echo
+echo 
+echo --------------------------------------------------------
+echo
+echo --- Grafana initial password follows ---
+echo
+kubectl get secret --namespace monitoring monitoring-grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
+echo
+echo ----------------------------------------
+echo
