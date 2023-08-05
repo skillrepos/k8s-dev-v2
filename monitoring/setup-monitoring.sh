202a6a1e70a87193e240d12058e8e9f20c3d5dbb
gwstudent <30451102+gwstudent@users.noreply.github.com>
2023-08-05 14:55:55 -0400

    Update setup-monitoring.sh

--- monitoring/setup-monitoring.sh
+++ monitoring/setup-monitoring.sh
@@ -12,17 +12,4 @@ echo
 # install dashboard
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.5.0/aio/deploy/recommended.yaml
 kubectl apply -f /workspaces/k8s-dev-v2/monitoring/dashboard-rbac.yaml
-echo
-echo ---- TOKEN to use for logging into dashboard follows ---
-echo 
-/workspaces/k8s-dev-v2/monitoring/get-token.sh
-echo
-echo 
-echo --------------------------------------------------------
-echo
-echo --- Grafana initial password follows ---
-echo
-kubectl get secret --namespace monitoring monitoring-grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
-echo
-echo ----------------------------------------
-echo
+
