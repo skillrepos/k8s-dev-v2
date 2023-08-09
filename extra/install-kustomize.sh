acb684cf0843803da807cb69ffed0f08f31879bd
Brent Laster <bclaster@nclasters.org>
2023-08-08 20:36:35 -0400

    Create install-kustomize.sh

---
+++ extra/install-kustomize.sh
@@ -0,0 +1,4 @@
+# install kustomize
+curl --silent --location --remote-name https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize/v3.5.4/kustomize_kustomize.v3.5.4_linux_amd64 
+chmod a+x kustomize_kustomize.v3.5.4_linux_amd64 
+sudo mv kustomize_kustomize.v3.5.4_linux_amd64 /usr/local/bin/kustomize
