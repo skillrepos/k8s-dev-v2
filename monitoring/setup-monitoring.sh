b84907de1f4afa28b01e5e5f1a67d23258006956
Brent Laster <bclaster@nclasters.org>
2023-08-08 23:17:41 -0400

    Update setup-monitoring.sh

--- monitoring/setup-monitoring.sh
+++ monitoring/setup-monitoring.sh
@@ -5,14 +5,10 @@ echo ...Installing Prometheus and Grafana
 echo
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
-helm install -n monitoring monitoring --version="38.0.3" prometheus-community/kube-prometheus-stack
+helm install -n monitoring monitoring --version="38.0.3" prometheus-community/kube-prometheus-stack --set prometheusOperator.admissionWebhooks.failurePolicy=Ignore
 echo
 echo ...Installing Kubernetes dashboard
 echo
 # install dashboard
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.5.0/aio/deploy/recommended.yaml
 kubectl apply -f /workspaces/k8s-dev-v2/monitoring/dashboard-rbac.yaml
-# install kustomize
-curl --silent --location --remote-name \"https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize/v3.5.4/kustomize_kustomize.v3.5.4_linux_amd64" 
-chmod a+x kustomize_kustomize.v3.5.4_linux_amd64 
-sudo mv kustomize_kustomize.v3.5.4_linux_amd64 /usr/local/bin/kustomize
